9536a5f6bac1f0b1a2961614f7c09cc286c337af
wijnandK <korlaarwijnand@hotmail.com>
2022-09-20 09:25:31 +0200

    Worklist

--- src/App.js
+++ src/App.js
@@ -2,7 +2,19 @@ import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  return <div className="App">``</div>
+  return (
+    <div className="App">
+      {/* Add login screen, register screen */}
+      {/* add Form  hook to send credentials to api */}
+      {/* if registering  then save user */}
+      {/* Jwt token returned add too local storage */}
+      {/* create user state */}
+      {/* name, id , email, auth, isAdmin */}
+      {/* Load the products from the api  in the app & all querry*/}
+      {/* Create page for a single product item, and productlist */}
+      {/* setup productstate , cart start */}
+    </div>
+  )
 }
 
 export default App
