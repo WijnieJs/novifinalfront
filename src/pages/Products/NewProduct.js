fc42922937cb1d2c5a17feadb8881c80f7ebb1eb
wijnandK <korlaarwijnand@hotmail.com>
2022-09-25 01:38:54 +0200

    Add reducer context and localstorage load data

--- src/pages/Products/NewProduct.js
+++ src/pages/Products/NewProduct.js
@@ -1,23 +1,12 @@
 import React, { useContext, useState } from 'react'
-import { useHistory } from 'react-router-dom'
-import ErrorModal from '../../components/UI-interfaces/ErrorModal'
-import axios from 'axios'
 import Button from '../../components/Forms/Button'
 import Input from '../../components/Forms/Input'
-<<<<<<< HEAD
-=======
 
->>>>>>> copyone
 import { VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH } from '../../utils/validators'
 
 import { useForm } from '../../utils/Hooks/form-hook'
 import ProductContext from '../../utils/context/products-context'
 import './ProductForm.css'
-<<<<<<< HEAD
-import { addProduct } from '../../utils/Context/product/ProductAction'
-import { ProductContext } from '../../utils/Context/product/ProductContext'
-import { useHttpClient } from '../../utils/hooks/http-hook'
-=======
 // import { addProduct } from '../../utils/Context/product/ProductAction'
 
 // {
@@ -26,12 +15,10 @@ import { useHttpClient } from '../../utils/hooks/http-hook'
 //     "published" : true,
 //     "price" : 11.2
 // }
->>>>>>> copyone
-
 const NewProduct = () => {
-  const { isLoading, error, sendRequest, clearError } = useHttpClient()
-  const products = useContext(ProductContext)
-  const [errorMsg, setErrorMsg] = useState()
+  const { products, loading, error, repos, dispatch } = useContext(
+    ProductContext,
+  )
 
   const [formState, inputHandler] = useForm(
     {
@@ -48,19 +35,13 @@ const NewProduct = () => {
         isValid: false,
       },
       price: {
-        value: '',
+        value: Number,
         isValid: false,
       },
     },
     false,
   )
-  const history = useHistory()
-  const data = {
-    title: formState.inputs.title.value,
-    description: formState.inputs.description.value,
-    price: formState.inputs.price.value,
-    publised: formState.inputs.publised.value,
-  }
+
   const productSubmitHandler = async (e) => {
     e.preventDefault()
     dispatch({ type: 'SET_LOADING' }, true)
@@ -71,15 +52,8 @@ const NewProduct = () => {
       price: formState.inputs.price.value,
       publised: formState.inputs.publised.value,
     }
-<<<<<<< HEAD
-    dispatch({ type: 'SET_ERROR' }, false)
-
-    try {
-      const response = await addProduct(data)
-=======
     let response
     try {
->>>>>>> copyone
       let msg = response
       // console.log(response)
 
@@ -92,11 +66,6 @@ const NewProduct = () => {
 
   return (
     <React.Fragment>
-<<<<<<< HEAD
-      <ErrorModal error={errorMsg} onClear={clearError} />
-
-=======
->>>>>>> copyone
       <form className="product-form" onSubmit={productSubmitHandler}>
         {/* {isLoading && <LoadingSpinner asOverlay />} */}
         <Input
@@ -125,15 +94,6 @@ const NewProduct = () => {
           validators={[VALIDATOR_REQUIRE()]}
           errorText="Please enter a valid address."
           onInput={inputHandler}
-        />
-
-        <Input
-          type="number"
-          element="input"
-          label="Enter a price"
-          errorText="Please enter a price"
-          onInput={inputHandler}
-          validators={[VALIDATOR_REQUIRE()]}
         />
         {/* <ImageUpload
           id="image"
