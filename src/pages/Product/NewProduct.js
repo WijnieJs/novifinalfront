41d3f9a6c1362f03b4e3550313a8a9331efb66e2
wijnandK <korlaarwijnand@hotmail.com>
2022-11-06 01:35:10 +0100

    setup imageloAD

--- src/pages/Product/NewProduct.js
+++ src/pages/Product/NewProduct.js
@@ -9,7 +9,6 @@ import { VALIDATOR_REQUIRE } from '../../shared/utils/validators';
 import Button from '../../components/Button/Button';
 import Auth from '../Auth/Auth';
 import ImageUpload from '../../components/Form/ImageUpload';
-import axios from 'axios';
 const NewProduct = () => {
    const navigate = useNavigate();
    const [file, setFile] = useState();
@@ -28,10 +27,6 @@ const NewProduct = () => {
          price: {
             value: 0,
             isValid: false
-         },
-         image: {
-            value: null,
-            isValid: false
          }
       },
       false
@@ -40,43 +35,24 @@ const NewProduct = () => {
    const errorAcceptHandler = () => {
       setError(false);
    };
-
-   const addProductSubmit = async (event) => {
+   const fileChangedHandler = (event) => {
+      const file = event.target.files[0];
+      setFile(file);
+   };
+   const uploadHandler = async (event) => {
       event.preventDefault();
-      const imageUrl = formState.inputs.image.value;
-      const sending = {
-         name: imageUrl.name,
-         type: imageUrl.type
-      };
-
-      const data = new FormData();
-      data.append('image', formState.inputs.image.value);
-      data.append('title', formState.inputs.title.value);
-      data.append('description', formState.inputs.description.value);
-      data.append('price', formState.inputs.price.value);
-
       try {
-         const result = await axios.post(
-            'http://localhost:8080/api/public/newproduct',
-            data,
-            {
-               headers: {
-                  'Content-Type': 'multipart/form-data'
-               }
-            }
-         );
-         console.log(result.data);
+         const formData = {
+            title: formState.inputs.title.value,
+            description: formState.inputs.description.value,
+            price: formState.inputs.price.value,
+            file: file
+         };
+         await ProductActions.add(formData);
 
-         // const formData = {
-         //    title: formState.inputs.title.value,
-         //    description: formState.inputs.description.value,
-         //    price: formState.inputs.price.value,
-         //    imageURL: sending
-         // };
-
-         // await ProductActions.add(data);
-         // navigate('/');
+         console.log('Trough');
       } catch (error) {
+         console.log(error);
          if (error.response.data) {
             setErrorMessage(Object.values(error.response.data));
             setError(true);
@@ -98,7 +74,7 @@ const NewProduct = () => {
          {formState.inputs.image &&
             console.log(formState.inputs.image.value)}
 
-         <form onSubmit={addProductSubmit}>
+         <form onSubmit={uploadHandler}>
             <Input
                element='input'
                id='title'
@@ -108,7 +84,6 @@ const NewProduct = () => {
                errorText='Please enter a title.'
                onInput={inputHandler}
             />
-
             <Input
                element='input'
                id='description'
@@ -127,13 +102,20 @@ const NewProduct = () => {
                errorText='Please enter a price'
                onInput={inputHandler}
             />
-            <ImageUpload
+            <input
+               type='file'
+               name='image-field'
+               id='student-image'
+               onChange={fileChangedHandler}
+            />
+            {/* <ImageUpload
                id='image'
                onInput={inputHandler}
                errorText='Please provide an image.'
-            />
-            <Button type='submit' disabled={!formState.isValid}>
-               ADD
+            /> */}
+            =
+            <Button mode='raised' onClick={uploadHandler}>
+               Submit
             </Button>
          </form>
       </Auth>
